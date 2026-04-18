c6a5b8d66e97144fde72716b4e0dd6b8a7390d22
Akihiro Fujii <inuinuhikaru0802@gmail.com>
2026-04-18 13:34:49 +0900

    Normalize direct index.html access before router startup

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,5 +3,9 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
+  if (window.location.pathname === "/index.html") {
+    window.history.replaceState({}, "", "/");
+  }
+
   createRoot(document.getElementById("root")!).render(<App />);
   
